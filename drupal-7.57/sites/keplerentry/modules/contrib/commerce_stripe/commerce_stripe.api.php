8733fad467b99156bcd33ebfb23d64a5b3edbd26
Andrew Pealing - norwegian.blue <roo@brod.keplerchallenge.co.nz>
2018-07-17 11:11:40 +1200

    Latest version of commerce-stripe installed. VBO installed to enable mass removal of abandoned carts from previous years

--- drupal-7.57/sites/keplerentry/modules/contrib/commerce_stripe/commerce_stripe.api.php
+++ drupal-7.57/sites/keplerentry/modules/contrib/commerce_stripe/commerce_stripe.api.php
@@ -6,6 +6,10 @@
 
 /**
  * Add information to the metadata sent to Stripe.
+ *
+ * The settings array for the payment method instance used to process charges
+ * for the order may be accessed via a temporary property added to the order
+ * object for this hook: $order->payment_method_settings
  */
 function hook_commerce_stripe_metadata($order) {
   return array(
@@ -13,9 +17,12 @@ function hook_commerce_stripe_metadata($order) {
   );
 }
 
-
 /**
  * Alter the description of the order sent to Stripe in the payment details.
+ *
+ * The settings array for the payment method instance used to process charges
+ * for the order may be accessed via a temporary property added to the order
+ * object for this hook: $order->payment_method_settings
  */
 function hook_commerce_stripe_order_charge_alter(&$charge, $order) {
   // Example of alteration of the description.
@@ -23,6 +30,19 @@ function hook_commerce_stripe_order_charge_alter(&$charge, $order) {
     $card_id = rand(1, 10000);
     $charge['description'] = t('Token card id: %token_card_id', array('%token_card_id' => $card_id));
   }
-  // Add application fees.
-  $charge['application_fee'] = rand(1, 1000);
+
+  // Check to see if the site-wide Stripe Connect account is being used.
+  if (!empty($order->payment_method_settings['use_connected_account']) &&
+    $order->payment_method_settings['use_connected_account'] == 'site account') {
+    // Then add application fees.
+    $charge['application_fee'] = rand(1, 1000);
+  }
+}
+
+/**
+ * Alter the reason of the refund.
+ */
+function hook_commerce_stripe_order_refund_alter(&$refund, $form_state) {
+  // Example of alteration of the reason.
+  $refund['reason'] = t('duplicate');
 }
